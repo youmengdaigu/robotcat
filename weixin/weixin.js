74f6bd0d0d5c9d5215dda61ac10d6da5f670597a
rickyall <783814127@qq.com>
2015-12-01 14:23:36 +0800

    gengxin

--- weixin/weixin.js
+++ weixin/weixin.js
@@ -21,8 +21,15 @@ module.exports = function(app){
         //判断用户是否注册如果没有注册则注册用户
         weixinLogin(openID,function(user){
           req.session.user = user;
+          console.log(user);
           console.log(req.session.user);
           console.log('登陆成功！');
+        });
+      }else if(message.Event === "VIEW"){
+        var openID = message.FromUserName;
+        weixinLogin(openID,function(user){
+          req.session.user = user;
+          res.redirect('/');
         });
       }
     }
